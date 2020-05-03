4a1d0e04f2b346e1b7556ea3be2597db31cf31f6
Matthew Gigante <mgiga0420@gmail.com>
2020-05-03 13:42:05 -0400

    Added UI for player health, hunger, and thirst. bars do not deplete yet.

---
+++ TheMysteriousGrove/Assets/Scripts/Player/PlayerHungerBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHungerBar : MonoBehaviour
+{
+    public GameObject playerObject;
+    public PlayerMovement playerMovement;
+    public Image fillImage;
+    public Slider slider;
+
+    void Start()
+    {
+        Invoke("FindPlayer", 0.2f);
+    }
+
+    public void FindPlayer()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = playerObject.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if (playerMovement != null)
+        {
+            slider.value = playerMovement.playerHunger;
+        }
+
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        else
+        {
+            fillImage.enabled = true;
+        }
+    }
+}
